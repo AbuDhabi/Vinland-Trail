2e587c7d3ab8832ded82e491a59e3ff2b4bdbb2e
Abu Dhabi <adhabi@gmail.com>
2012-10-19 19:37:22 +0200

    Some tweaks

--- high_seas.cpp
+++ high_seas.cpp
@@ -61,13 +61,15 @@ int high_seas() {
         } // end of message processing
         
         
-        if (frame==0) {
+        if (frame%2==0) {
             print_full_picture(BITMAPS[IMG_HIGH_SEAS_1]);
-            if (!PAUSED) frame = 1;
+            if (frame<0) frame = 0;
+            if (!PAUSED) frame++;
         }
-        else if (frame==1) {
+        else if (frame%2==1) {
             print_full_picture(BITMAPS[IMG_HIGH_SEAS_2]);
-            if (!PAUSED) frame = 0;
+            if (frame<0) frame = 1;
+            if (!PAUSED) frame++;
         }
         char temp[80];
         sprintf(temp,"Miles sailed: %d",DISTANCE);
@@ -77,17 +79,20 @@ int high_seas() {
         
         int skill_mod = 0; // sailors gonna sail
         if (CLASS == SAILOR) skill_mod = 10;
+        float skeleton_crew_mod = 1; // not enough crew?
+        if (how_many_alive()+INVENTORY.settlers < 5) skeleton_crew_mod = (how_many_alive()+INVENTORY.settlers)/5.0;
         
-        if (!PAUSED) {
+        if (!PAUSED && frame%5 == 0) {
+            
             if (PACE == 0) {
-                DISTANCE += (int)(36 + rand()%6 - rand()%6 + skill_mod/2)*(INVENTORY.boat_status/100.0);
+                DISTANCE += (int)(36 + rand()%6 - rand()%6 + skill_mod/2)*(INVENTORY.boat_status/100.0)*skeleton_crew_mod;
                 for (int i=0;i<5;i++) {
                     change_health(i,1);
                 }
             } else if (PACE == 1) {
-                DISTANCE += (int)(72 + rand()%12 - rand()%12 + skill_mod)*(INVENTORY.boat_status/100.0); // from SRD, 3mi/hr
+                DISTANCE += (int)(72 + rand()%12 - rand()%12 + skill_mod)*(INVENTORY.boat_status/100.0)*skeleton_crew_mod; // from SRD, 3mi/hr
             } else if (PACE == 2) {
-                DISTANCE += (int)(108 + rand()%18 - rand()%18 + skill_mod*2)*(INVENTORY.boat_status/100.0);
+                DISTANCE += (int)(108 + rand()%18 - rand()%18 + skill_mod*2)*(INVENTORY.boat_status/100.0)*skeleton_crew_mod;
                 for (int i=0;i<5;i++) {
                     change_health(i,-1);
                 }
@@ -104,24 +109,40 @@ int high_seas() {
                 INVENTORY.food -= INVENTORY.settlers;
             } else if (RATIONS == 2) {
                 for (int i=0;i<5;i++) {
-                    if (PARTY[i].health > 0) { INVENTORY.food--; } // normal
-                    if (PARTY[i].health > 0) { INVENTORY.food--; } // normal
+                    if (INVENTORY.food >= 2) {
+                         if (PARTY[i].health > 0) { INVENTORY.food -= 2; }
+                    } else {
+                        change_health(i,-1);
+                    }
                 }
-                INVENTORY.food -= INVENTORY.settlers;
-                INVENTORY.food -= INVENTORY.settlers;
+                for (int i=0;i<INVENTORY.settlers;i++) {
+                    if (INVENTORY.food >=2) {
+                        INVENTORY.food -= 2;
+                    } else {
+                        if (rand()%100 == 0) {
+                            INVENTORY.settlers--;
+                        }
+                    }
+                }
             } else if (RATIONS == 3) {
                 for (int i=0;i<5;i++) {
-                    if (PARTY[i].health > 0) { INVENTORY.food--; } // sir DOUBLE FAGGOT
-                    if (PARTY[i].health > 0) { INVENTORY.food--; } 
-                    if (PARTY[i].health > 0) { INVENTORY.food--; } // sir DOUBLE FAGGOT
-                    if (PARTY[i].health > 0) { INVENTORY.food--; } 
-                    change_health(i,1);
+                    if (INVENTORY.food >= 4) {
+                         if (PARTY[i].health > 0) { INVENTORY.food -= 4; }
+                         change_health(i,1);
+                    } else {
+                        change_health(i,-1);
+                    }
                 }
-                INVENTORY.food -= INVENTORY.settlers;
-                INVENTORY.food -= INVENTORY.settlers;
-                INVENTORY.food -= INVENTORY.settlers;
-                INVENTORY.food -= INVENTORY.settlers;
-            } // TODO: edge cases, such as eating when dead, or eating last crumbs
+                for (int i=0;i<INVENTORY.settlers;i++) {
+                    if (INVENTORY.food >=4) {
+                        INVENTORY.food -= 4;
+                    } else {
+                        if (rand()%100 == 0) {
+                            INVENTORY.settlers--;
+                        }
+                    }
+                }
+            } 
             DAYS++;
             ALREADY_FISHEDORWHALED = false;
             
@@ -135,6 +156,7 @@ int high_seas() {
             else if (DISTANCE > 1000 && LANDMARKS == 0) { LANDMARKS++; return 11; } // bergen
             
         }// else { // if paused
+        
         char pace[80];
         if (PACE == 0) strcpy(pace,"slow");
         else if (PACE == 1) strcpy(pace,"even");
@@ -182,7 +204,7 @@ int high_seas() {
         
         
         // sanity checks
-        if (INVENTORY.food < 1) { RATIONS = 0; } // why can't I feed all these mouths?
+        //if (INVENTORY.food < 1) { RATIONS = 0; } // not necessary anymore
         
         // let's see those game-overs
         if (INVENTORY.boat_status <= 0) return 6; // game over, boat crashed
@@ -197,7 +219,7 @@ int high_seas() {
         if (PAUSED) 
             SDL_Delay(100);
         else    
-            SDL_Delay(500);  // TODO: adjust this properly, so it's not too slow and not too fast
+            SDL_Delay(100);  // TODO: adjust this properly, so it's not too slow and not too fast
     }
     
     return 0;
